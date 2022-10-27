103d6ad3c10d63fd2e7cbf0b2732a3d032e8404e
menbatisiunissart <aboulenger@gmail.com>
2022-10-27 10:05:01 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TransLOB',
+    version='0.0.1',
+    description='PyTorch implementation of TransLOB.',
+    author='',
+    author_email='',
+    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url='https://github.com/menbatisiunissart/translob',
+    install_requires=['torch'],
+    packages=find_packages(),
+)
+
